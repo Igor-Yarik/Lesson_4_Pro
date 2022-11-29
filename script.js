930792d4ca508b86be799ba2aa4118f60c83cbc6
Igor Yaroshovets <igor.yarik@gmail.com>
2022-11-29 22:04:25 +0200

    start

---
+++ script.js
@@ -0,0 +1,26 @@
+/* let str = 'I am string';
+str[str.length - 1]
+console.log(str[str.length - 2]) */
+
+/* str.length - выводит количество символов */
+/* str.includes - проверка наявности слова */
+/* str.search - выводит с какой позиции начинается слово (значение) */
+/* str.slice - возвращает строку (значение) от начала и до (не включая - до) */
+/* str.replace - Изменяет подстроку на подстроку (слово на слово) */
+
+/* let str = "Please locate where locate occurs!";
+let pos = str.includes("locate  ");
+str.search("occurs")
+console.log(search) */
+
+/* let str = "stringify";
+str.slice(0,1);
+console.log(str.slice) */
+
+/* str = "lorem ipsum is simply dummy";
+let newStr = str.replace("lorem", "Lorem"); // Please visit W3Schools! */
+
+/* const string = "lorem lpsum is simply dummy";
+const stringL = string.replace(string[4], string[4].toUpperCase());
+console.log(stringL) */
+
